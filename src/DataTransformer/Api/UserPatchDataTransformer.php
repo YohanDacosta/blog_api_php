8a73a43a407b825ae2f3dec1cd59ee74af5fb36f
Yohan910 <yohan.dacosta910@gmail.com>
2025-08-19 22:15:17 +0200

    Commited files DataTransformer and State/Processor/User

---
+++ src/DataTransformer/Api/UserPatchDataTransformer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataTransformer\Api;
+
+use App\Entity\User;
+use App\Dto\Api\UserDto;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use ApiPlatform\Validator\ValidatorInterface;
+
+class UserPatchDataTransformer
+{
+    public function __construct(
+        private ValidatorInterface $validator,
+        private UserPasswordHasherInterface $userPasswordHasher
+    )
+    {}
+
+    public function transform(UserDto $data, User $user)  
+    {
+
+        if($data && $data->getFirstname() != null) {
+            $user->setFirstname($data->getFirstname());
+        } 
+
+        if($data && $data->getLastname() != null) {
+            $user->setLastname($data->getLastname());
+        } 
+
+        if($data && $data->getEmail() != null) {
+            $user->setEmail($data->getEmail());
+        } 
+
+        if ($data && $data->getPassword() != null) {
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, $data->getPassword()));
+        }
+
+        return $user;
+    }
+}
---
+++ src/State/Processor/User/UserPostProcessor.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\State\Processor\User;
+
+use App\Dto\Api\UserDto;
+use App\DataTransformer\Api\UserPostDataTransformer;
+use App\State\Processor\User\PersistProcessorInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+
+class UserPostProcessor implements PersistProcessorInterface
+{
+    public function __construct(
+        private UserPostDataTransformer $userDataTransformer,
+        private ProcessorInterface $persistProcessor,
+    )
+    {}
+
+    public function process(
+        mixed $data, 
+        Operation $operation, 
+        array $uriVariables = [], 
+        array $context = []
+    )
+    {
+        if (!$data instanceof UserDto) {
+            return $data;
+        }
+        $user = $this->userDataTransformer->transform($data);
+
+    	$this->persistProcessor->process($user, $operation, $uriVariables, $context);
+
+        return $user;
+    }
+}
